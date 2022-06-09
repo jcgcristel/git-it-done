88dd67b5a74f1fbb94cd3ced7ecf4c558b2029fe
jcg <jcg.cristel@gmail.com>
2022-06-08 23:45:29 -0400

    dynamically fetch repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,12 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos")
-}
+var getUserRepos = function(user) { var response = fetch(`https://api.github.com/users/${user}/repos`).then(function(response) {
+    console.log("fetch ---");
+    console.log(response);
+    
 
-getUserRepos();
+    response.json().then(function(data) {
+        console.log("data ----")
+        console.log(data);
+    });
+})};
+
+getUserRepos("octocat");
